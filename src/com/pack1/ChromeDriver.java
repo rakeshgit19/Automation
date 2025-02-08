e50ac04c98cc86aca7b5064a37c73bc0e34c9812
pc <pc@DESKTOP-2VBBHL1>
2025-02-08 14:51:04 +0530

    Initial commit

---
+++ src/com/pack1/ChromeDriver.java
@@ -0,0 +1,19 @@
+package com.pack1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ChromeDriver {
+
+	public static void main(String[] args) 
+	{
+		//System.out.println("i am not getting the solution");
+		//ChromeDriver driver = new ChromeDriver();
+		WebDriver dr = new org.openqa.selenium.chrome.ChromeDriver();
+		//WebDriver dr = new FirefoxDriver();
+		//dr.get("facebook.com");
+		
+
+	}
+
+}
